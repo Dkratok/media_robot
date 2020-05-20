f20305dbd270d3acd7966bea1091ebcb294398bd
Dkratok <Dkratok@gmail.com>
2020-05-20 12:32:22 +0300

    message

--- src/tests/test_BaseUIScript.py
+++ src/tests/test_BaseUIScript.py
@@ -4,23 +4,23 @@ from src.pages.instagram.login_dialogs import NotificationDialog
 from src.core.driver_run import DriverRun
 
 
-class BaseUIScriptA(unittest.TestCase):
+class BaseUIScript(unittest.TestCase):
 
     def setUp(self):
         driver_run = DriverRun()
-        driver = driver_run.run_driver()
-        driver.maximize_window()
-        driver.get("https://instagram.com")
-        instagramLoginPage = InstagramLoginPage(driver)
+        self.driver = driver_run.run_driver()
+        self.driver.maximize_window()
+        self.driver.get("https://instagram.com")
+        instagramLoginPage = InstagramLoginPage(self.driver)
         instagramLoginPage.login()
-        notificationsDialog = NotificationDialog(driver)
+        notificationsDialog = NotificationDialog(self.driver)
         notificationsDialog.notifications_skip()
 
     def test_script(self):
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
     def tearDown(self):
-        """self.driver.close()"""
+        self.driver.close()
 
 
 if __name__ == "__main__":
